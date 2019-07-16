a5c773610b64ba43af967af8f1a474139cb7a3e5
linliangbin <linliangbin@ruijie.com.cn>
2019-07-16 11:09:58 +0800

    增加工具类

---
+++ app/src/main/java/com/serenegiant/log/FrameLogger.java
@@ -0,0 +1,35 @@
+package com.serenegiant.log;
+
+import android.util.Log;
+
+import java.util.HashMap;
+
+public class FrameLogger {
+
+    static class TagFrame {
+        int count;
+        long startTime;
+    }
+
+    HashMap<String, TagFrame> tagMap = new HashMap<>();
+
+    public synchronized void frame(String tag) {
+        long currentTime = System.currentTimeMillis();
+        TagFrame tagFrame = tagMap.get(tag);
+        if (tagFrame == null) {
+            tagFrame = new TagFrame();
+            tagFrame.count = 0;
+            tagFrame.startTime = currentTime;
+            tagMap.put(tag, tagFrame);
+        } else {
+            tagFrame.count++;
+            if (currentTime - tagFrame.startTime >= 1000) {
+                Log.i("FrameLogger", tag + " fps= " + tagFrame.count);
+                tagFrame.count = 0;
+                tagFrame.startTime = currentTime;
+            } else {
+//                Log.i("FrameLogger", tag + " frame " +tagFrame.count);
+            }
+        }
+    }
+}
